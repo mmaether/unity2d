2563b56066695eff20dc0c52bccc2ff1a98c30d6
Mike Maether <mike.maether@gmail.com>
2019-06-04 23:24:27 -0400

    Migrated Unity2d repo to GitHub.

---
+++ README.md
@@ -0,0 +1,5 @@
+# Unity 2D
+
+This repo will hold all projects worked on for Udemy's [Complete C# Unity Developer 2D: Learn to Code Making Games](https://www.udemy.com/unitycourse/) course by Ben Tristem. I've always been interested with game development and design, and finally decided to take a stab at it.
+
+I expect to include notes here for myself to reference, keeping everything maintained within the repo. There will be some obvious beginner notes here which I hope may be useful to look back at.
---
+++ Text101/Text101/Assets/Scripts/AdventureGame.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+/* 
+ * Order for putting fields together.
+ * 1. Right click in Hierarchy menu, create Empty. This is a blank GameObject we can customize.
+ * 2. We want to use it for scripts. Select Game in Hierarchy menu, then Add Component. Select New Script, enter a name.
+ * 3. We now have a script, except it isn't link. Open it in VS (here). Then enter the SerializeField line. This makes the field appear in the Unity UI.
+ * 4. Since we label it as Text, we can only select Text elements. Select the text field we want to dynamically update.
+ * 5. Now we can reference the textComponent and update the text programmatically.
+ */
+
+/*
+ * You wake up in a room with white walls and a blinding light that immediately strains your eyes. You don't remember
+ * who you are or how you got there. You notice a light switch and a note pinned to the wall.
+ * 
+ * 1. Flip the light switch.
+ * 2. Read the note.
+ */
+
+public class AdventureGame : MonoBehaviour {
+
+    // [SerializeField] allows this variable to be seen in the Unity UI inspector.
+    [SerializeField] Text textComponent;
+    [SerializeField] State startingState;
+
+    State state;
+
+	// Use this for initialization
+	void Start () {
+        state = startingState;
+        textComponent.text = state.GetStateStory();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        ManageState();
+	}
+
+    private void ManageState()
+    {
+        var nextStates = state.GetNextStates();
+        for (int i = 0; i < nextStates.Length; i++) {
+            if (Input.GetKeyDown(KeyCode.Alpha1 + i))
+            {
+                state = nextStates[i];
+            }
+        }
+        textComponent.text = state.GetStateStory();
+    }
+}
